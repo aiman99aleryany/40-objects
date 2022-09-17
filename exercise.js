3ac8944b16a05677e2bf6834e99a2dda894929b9
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-18 02:55:53 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,9 @@
+// Create an object literal called car with two properties: name and color. Print in the console the values of the properties.
+
+let car = {
+  name: "Aston Martin",
+  color: "Grey",
+};
+
+console.log(car.name);
+console.log(car.color);
